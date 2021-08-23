96e7e823d74388bc63b66e0db49c79a8138691b8
Pedro Martins <pedrovrima@gmail.com>
2021-08-23 11:20:26 -0300

    adds login

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Context, ContextProvider } from "./context";
 import {SaltatorRouter} from "./router";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 function App() {
+  console.log(window.location.origin)
   return (
+    <Auth0Provider
+    domain="dev-skphwm08.us.auth0.com"
+    clientId="5AkjixQWOlv1FQpzboje929M5eJepDDP"
+    redirectUri={window.location.origin}
+  >
+
+    
     <ContextProvider>
       <SaltatorRouter />
     </ContextProvider>
-  );
+   </Auth0Provider>);
 }
 
 export default App;
--- src/components/entry.tsx
+++ src/components/entry.tsx
@@ -1,13 +1,16 @@
 import React, { useContext } from "react";
 import { Context, contextType } from "../context";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
 const Entry: React.FC = () => {
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <>
       <div
         style={{
-          backgroundImage:
-            "url(https://sites.unicentro.br/wp/manejoflorestal/files/2019/11/T.jpg",
+          backgroundImage:            "url(https://sites.unicentro.br/wp/manejoflorestal/files/2019/11/T.jpg)",
           backgroundRepeat: "no-repeat",
           backgroundSize: "fill",
         }}
@@ -22,23 +25,57 @@ const Entry: React.FC = () => {
           <h1 className="text-3xl sm:text-7xl text-gray-200 font-extrabold font-sans">
             Aprenda os cantos das aves brasileiras
           </h1>
-          <Link to="/login">
-            <button className="px-4 py-2 bg-emerald-400 rounded-lg font-bold text-gray-700 shadow-lg m-4 font-md sm:text-lg">
+          {/* <Link to="/login"> */}
+            <button  onClick={() => loginWithRedirect()} className="px-4 py-2 bg-emerald-600 hover:bg-emerald-900 rounded-lg font-bold text-gray-200 shadow-lg m-4 font-md sm:text-lg">
               Comece já!{" "}
             </button>
-          </Link>
+          {/* </Link> */}
         </div>
       </div>
       <div className="">
-        <h1 className="text-center font-bold font-gray-900 text-2xl sm:text-4xl mt-4 p-2">Como Funciona</h1>
-<div className="mx-24 grid grid-cols-3  mt-6">
-  <p className="mr-12 text-2xl">Selecione um deck de espécies para começar a estudar, montados com base na localização e raridade das espécies</p>
-  <p className="mr-12 text-2xl">Comece seus estudos: ouça o canto tocado e selecione a espécie que você acha que é a correspondente ao canto</p>
-        <p className="mr-12 text-2xl">Baseado na </p>
+        <h1 className="text-center font-bold font-gray-900 text-2xl sm:text-4xl mt-4 p-2">
+          Como Funciona
+        </h1>
+        <div className="mx-4 sm:mx-24 grid sm:grid-cols-3  mt-6">
+          <div className="mb-4 sm:mr-12 ">
+            <h2 className="text-lg font-bold">Selecione um deck</h2>
+            <p className="text-md">
+              Escolha um grupo espécies para começar a estudar, montados com
+              base na localização e raridade das espécies
+            </p>
+          </div>
+          <div className="mb-4 sm:mr-12 ">
+            <h2 className="text-lg font-bold"> Tente identificar os sons</h2>
+            <p className="text-md">
+              Ouça o canto tocado e selecione a espécie que você acha que é a
+              correspondente ao canto{" "}
+            </p>
+          </div>
 
-</div>
+          <div className="mb-4 sm:mr-12 ">
+            <h2 className="text-lg font-bold"> Continue estudando</h2>
+            <p className="text-md">
+              Seus futuros estudos serão baseados em seus acerto e erros,
+              otimizando o aprendizado.{" "}
+            </p>
+          </div>
+        </div>
 
-      
+        <div className="m-12 flex sm:flex-row justify-around">
+          <div className="flex flex-col items-center">
+            <h2 className="text-lg font-bold"> Realização</h2>
+            <img src="https://www.oama.eco.br/_next/image?url=%2Flogo.png&w=64&q=75"></img>
+          </div>
+          <div className="flex flex-col items-center">
+            <h2 className="text-lg font-bold col"> Apoio</h2>
+            <div className="flex flex-row justify-center items-center">
+            <img className="h-16 w-16" src="https://www.oama.eco.br/_next/image?url=%2Fpartners%2Fbirds%20atlantic.jpg&w=1920&q=75"></img>
+            <img className="h-12 w-12" src="https://crbio04.gov.br/wp-content/uploads/2020/07/logo-crbio04.png"></img>
+            </div>
+          </div>
+
+
+        </div>
       </div>
     </>
   );
